b9942d0ad406c2e8b0660a6bbec2612793747bd9
Hadi Alqattan <alqattanhadizaki@gmail.com>
2021-04-02 00:23:20 +0300

    More clean deploy.sh

--- dist/364.846472632ffb85b2ef2f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunktahsily_progress_calculator=self.webpackChunktahsily_progress_calculator||[]).push([[364],{364:(s,a,l)=>{"use strict";l.r(a)}}]);
--- dist/833.bc46d85e531f3bdd68ca.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunktahsily_progress_calculator=self.webpackChunktahsily_progress_calculator||[]).push([[833],{833:(s,a,l)=>{"use strict";l.r(a)}}]);
