e9aca632b42ef5c262359eb5f510e7d589116cf2
Hadi Alqattan <alqattanhadizaki@gmail.com>
2021-04-01 23:16:15 +0300

    setup deployment.

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "$1" ]]; then
+  echo "Which folder do you want to deploy to GitHub Pages?"
+  exit 1
+fi
+
+# $1 (e.g. dist)
+git subtree push --prefix $1 origin gh-pages
