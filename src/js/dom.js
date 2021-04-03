41a50b2c5fe75e673cc99e1566358ffa9bdf90ae
Hadi Alqattan <alqattanhadizaki@gmail.com>
2021-04-03 16:23:07 +0300

    Add: Arabic translation! (#9)
    * Add: Arabic support!
    
    * Add: plural/singular detection functionality
    
    * Add: change-language btn in settings.

--- src/js/dom.js
+++ src/js/dom.js
@@ -2,8 +2,28 @@
 This file contains core DOM manipulation functions.
 */
 
+import i18n from '../locales/i18n.js'
 import { getTotalRemainder } from './state.js'
 import { getPagesPerDay } from './math.js'
+
+/* Utilities (Unexported) */
+const getPagesWord = (pagesCount) => {
+  if (i18n.language == 'ar') {
+    return pagesCount > 1 && pagesCount < 11 ? 'pages' : 'page'
+  } else {
+    return pagesCount > 1 ? 'pages' : 'page'
+  }
+}
+const getDaysWord = (daysCount) => {
+  if (i18n.language == 'ar') {
+    return daysCount > 1 && daysCount < 11 ? 'days' : 'day'
+  } else {
+    return daysCount > 1 ? 'days' : 'day'
+  }
+}
+const getWord = (func, arg) => {
+  return i18n.t('words.' + func(arg))
+}
 
 /* update the given progressbar percentage.
 
@@ -16,29 +36,28 @@ const updateProgressbar = (elm, newPercentage) => {
 
 /* update subject-input value span */
 const updateInputValueSpan = (elm, currentValue) => {
-  elm.innerHTML = 'last page: ' + currentValue
+  elm.innerHTML = i18n.t('report.lastPage', { pageNum: currentValue })
 }
 
 /* update the given report span */
 const updateReportSpan = (elm, totalPages, remainedPages) => {
-  elm.innerHTML =
-    totalPages -
-    remainedPages +
-    ' out of ' +
-    totalPages +
-    ' pages, remained ' +
-    remainedPages +
-    (remainedPages > 1 ? ' pages' : ' page')
+  elm.innerHTML = i18n.t('report.subReport', {
+    done: totalPages - remainedPages,
+    total: totalPages,
+    remained: remainedPages,
+    pagesWord: getWord(getPagesWord, remainedPages),
+  })
 }
 
 /* update the given date remainder span */
 const updateDateRemainderSpan = (elm, daysDelta) => {
   let pagePerDay = getPagesPerDay(getTotalRemainder(), daysDelta)
-  let shouldStudy =
-    'study ' + pagePerDay + (pagePerDay > 1 ? ' pages' : ' page') + '/day'
-  let remainderDays =
-    daysDelta + (daysDelta > 1 ? ' days' : ' day') + ' remained'
-  elm.innerHTML = shouldStudy + ', ' + remainderDays
+  elm.innerHTML = i18n.t('report.date', {
+    pages: pagePerDay,
+    days: daysDelta,
+    pagesWord: getWord(getPagesWord, pagePerDay),
+    daysWord: getWord(getDaysWord, daysDelta),
+  })
 }
 
 /* update settings' last/first page input fields */
