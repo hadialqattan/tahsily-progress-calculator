e6ebaf2fd46dde5103367724d95d98767ac7bde8
Hadi Alqattan <alqattanhadizaki@gmail.com>
2021-04-03 15:01:00 +0300

    Add: plural/singular detection functionality

--- src/js/dom.js
+++ src/js/dom.js
@@ -6,6 +6,25 @@ import i18n from '../locales/i18n.js'
 import { getTotalRemainder } from './state.js'
 import { getPagesPerDay } from './math.js'
 
+/* Utilities (Unexported) */
+const getPagesWord = (pagesCount) => {
+  if (i18n.language == 'ar') {
+    return pagesCount > 1 && pagesCount < 11 ? 'pages' : 'page'
+  } else {
+    return pagesCount > 1 ? 'pages' : 'page'
+  }
+}
+const getDaysWord = (daysCount) => {
+  if (i18n.language == 'ar') {
+    return daysCount > 1 && daysCount < 11 ? 'days' : 'day'
+  } else {
+    return daysCount > 1 ? 'days' : 'day'
+  }
+}
+const getWord = (func, arg) => {
+  return i18n.t('words.' + func(arg))
+}
+
 /* update the given progressbar percentage.
 
 we assume the elm is a progressbar and has `w-xx` className at the end.
@@ -26,13 +45,19 @@ const updateReportSpan = (elm, totalPages, remainedPages) => {
     done: totalPages - remainedPages,
     total: totalPages,
     remained: remainedPages,
+    pagesWord: getWord(getPagesWord, remainedPages),
   })
 }
 
 /* update the given date remainder span */
 const updateDateRemainderSpan = (elm, daysDelta) => {
   let pagePerDay = getPagesPerDay(getTotalRemainder(), daysDelta)
-  elm.innerHTML = i18n.t('report.date', { pages: pagePerDay, days: daysDelta })
+  elm.innerHTML = i18n.t('report.date', {
+    pages: pagePerDay,
+    days: daysDelta,
+    pagesWord: getWord(getPagesWord, pagePerDay),
+    daysWord: getWord(getDaysWord, daysDelta),
+  })
 }
 
 /* update settings' last/first page input fields */
