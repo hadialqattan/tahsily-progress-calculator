cbaabc9b7cc0f81c3bfe5b963478a68cfc5cd67e
Hadi Alqattan <alqattanhadizaki@gmail.com>
2021-04-13 20:13:17 +0300

    SEO Improvements.

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -24,7 +24,12 @@ i18n
   })
   .then(function (t) {
     Array.from(document.getElementsByClassName('i18n')).forEach((elm) => {
-      elm.innerHTML = i18n.t(elm.getAttribute('data-i18n'))
+      let i18nT = i18n.t(elm.getAttribute('data-i18n'))
+      if (elm.nodeName == 'META') {
+        elm.setAttribute('content', i18nT)
+      } else {
+        elm.innerHTML = i18nT
+      }
     })
     Array.from(document.getElementsByClassName('dir')).forEach((elm) => {
       elm.dir = i18n.dir()
